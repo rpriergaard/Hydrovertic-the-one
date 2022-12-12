a3baf3a31cb9751f9442502ad11b90eff0b0c953
rpriergaard <rpmh@hotmail.dk>
2022-12-12 19:38:46 +0100

    full import
    importere alle filer

---
+++ dropdown.js
@@ -0,0 +1,16 @@
+var select = document.getElementById('subject');
+var option = ["generelt", "teknologi", "presse", "omos"];
+
+for (var i = 0; i < option.length; i++) {
+  var opt = options[i];
+  var el = document.createElement("option");
+  el.textContent = opt;
+  el.value = opt;
+  select.appendChild(el);
+}
+
+<
+select id = "subject" >
+  <
+  option > Vælg emne < /option> <
+  /select>
