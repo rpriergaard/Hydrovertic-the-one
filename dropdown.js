6d401a3e4ba3c03e8c58829d6d96af6e9ac2a550
rpriergaard <rpmh@hotmail.dk>
2022-12-16 12:49:40 +0100

    simone

--- dropdown.js
+++ dropdown.js
@@ -1,10 +1,14 @@
 var select = document.getElementById('subject');
+//array som går og vælger kategorierne/options fra HTML'en
 var option = ["generelt", "teknologi", "presse", "omos"];
-
+// options = object??
+//loop gennemgår array elementerne og laver en DOM til hvert element og så tilsættes objektet
 for (var i = 0; i < option.length; i++) {
+//variabel som giver "opt" assignment til "options"
   var opt = options[i];
   var el = document.createElement("option");
   el.textContent = opt;
   el.value = opt;
+//event der selecter en option
   select.appendChild(el);
 }
