804ae358a74cbe6883d17746989ea84504ac5eb3
rpriergaard <rpmh@hotmail.dk>
2022-12-16 13:45:56 +0100

    JS
    JS uden //

--- carousel.js
+++ carousel.js
@@ -1,71 +1,56 @@
-// Select all slides 
-//(array vil select alle class' med navnet .slide)(kan console.log for at vise array muligvis)
 const slides = document.querySelectorAll(".slide");
 
-// "loop" through slides and set each slides translateX property to index * 100% 
 slides.forEach((slide, indx) => {
   slide.style.transform = `translateX(${indx * 100}%)`;
 });
 
-// select next slide button
 const nextSlide = document.querySelector(".btn-next");
 
-// current slide counter
 let curSlide = 0;
-// laver en maximum number of slides, og så finder functionen og eventlisteneren nedenunder ud af hvor mange slides der skal være efter skærmens størrelse
 let maxSlide;
-// get window size
 let windowSize = window.innerWidth;
 
-//en function der læser størrelsen på skærmen der gå ind på siden og derefter sætter "max-slides" til at tilpasse
-// sig efter den rigtige størrelse.
-// så går eventlisteneren ind og ser om skærmen bliver resized, og så vil koden blive executed selv hvis skærmens størrelse ændres
-function setScreenSize(){
-  if (windowSize >= 786 && windowSize <= 1359){
+function setScreenSize() {
+  if (windowSize >= 786 && windowSize <= 1359) {
     maxSlide = 3;
   } else if (windowSize >= 1360) {
     maxSlide = 2;
-  } else if (windowSize <= 785) { 
+  } else if (windowSize <= 785) {
     maxSlide = slides.length - 1;
   }
 }
 window.addEventListener("load", (event) => {
-  setScreenSize(); 
+  setScreenSize();
 });
 
-window.addEventListener('resize', function(event){
-    windowSize = window.innerWidth;
-    setScreenSize(); 
-  });
- 
-// add event listener and navigation functionality
+window.addEventListener('resize', function (event) {
+  windowSize = window.innerWidth;
+  setScreenSize();
+});
+
 nextSlide.addEventListener("click", function () {
-// check if current slide is the last and reset current slide
+
   if (curSlide === maxSlide) {
     curSlide = 0;
   } else {
     curSlide++;
   }
 
-  //   move slide by -100%
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
 });
 
-// select prev slide button
 const prevSlide = document.querySelector(".btn-prev");
 
-// add event listener and navigation functionality
+
 prevSlide.addEventListener("click", function () {
-  // check if current slide is the first and reset current slide to last
   if (curSlide === 0) {
     curSlide = maxSlide;
   } else {
     curSlide--;
   }
 
-  //   move slide by 100%
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
--- slideshow.js
+++ slideshow.js
@@ -1,27 +1,26 @@
 let slideIndex = 0;
 showSlides();
-//funktionen der viser slidesne
+
 function showSlides() {
   let i;
-  //et array der går ind og vælger/finder alle objekter/class's med navnet "mySlides" og "dots" og bruger dem
-  //via en "DOM" som henter dem fra HTML
+
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  //Loop der skifter slidesne ud med hinanden når har haft en vis længde som vi sætter en timer på senere
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  //kontrolstruktur
+
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
-  //loop for dots så de følger slidesne, i small og medium skærm størrelse (fjernet på large)
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-  //timeren (event)
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
+
+  setTimeout(showSlides, 3000); 
 }
